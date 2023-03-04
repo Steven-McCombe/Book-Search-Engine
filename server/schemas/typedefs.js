8f4ecea9134079f148908854c3c409543d8f8a51
Steven-McCombe <smccombe93@gmail.com>
2023-03-04 15:30:44 -0500

    added mutation and auth to type def

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -1,6 +1,17 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  type Query {
+    me: [User]
+
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: savedBook!): User
+    removeBook(bookId: ID!): User
+  }
+
   type Book {
     bookId: String
     authors: [String]
@@ -17,11 +28,11 @@ const typeDefs = gql`
     bookCount: Int
     savedBooks: [Book]
   }
-
-  type Query {
-    user: [User]
-
+  type Auth {
+    token: ID!
+    user: User
   }
+
 `;
 
 module.exports = typeDefs;
