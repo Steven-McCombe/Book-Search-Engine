b333f086ffd9a8b2f9ddc402b3dc0fc3d83e1cd1
Steven-McCombe <smccombe93@gmail.com>
2023-03-05 22:43:50 -0500

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-
+//connect to mongodb atlas
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
