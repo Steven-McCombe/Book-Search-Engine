2b47779189d4f5bccf22f6203296830465212cc2
Steven-McCombe <smccombe93@gmail.com>
2023-03-06 19:23:57 -0500

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 //connect to mongodb atlas
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
